4698bdacf290ce534fcdd81e6420fd8cb4fba60a
sohammondal <soham.pf.mondal@gmail.com>
2018-11-14 11:02:27 +0530

    Used Template ERB & template resource in recipe

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -1,13 +1,16 @@
 package 'apache2'
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello World</h1>
-  <h3>#{node['hostname']}</h3>
-  <h3>#{node['ipaddress']}</h3>
-  <h3>#{node['hostnamectl']['operating_system']}</h3>
-"
-end
+#file '/var/www/html/index.html' do
+#  content "<h1>Hello World</h1>
+#  <h3>#{node['hostname']}</h3>
+#  <h3>#{node['ipaddress']}</h3>
+#  <h3>#{node['hostnamectl']['operating_system']}</h3>
+#"
+#end
 
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
+end
 
 service 'apache2' do 
   action [ :enable, :start]
